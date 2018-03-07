0f2a34437f03aa4cff189fc5c857fb588bfe3c0e
Eric Newsom <ericnews512@gmail.com>
2018-03-06 20:11:59 -0600

    Make server serve client's dist

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+const path = require('path')    // <-- added
+const config = require('./config/config')
+
 const app = express()
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../../client/dist')))    // <-- added
+
+app.listen(config.port)
+console.log(`Server started on port ${config.port}`)
